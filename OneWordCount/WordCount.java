1ecef7990c1bed97ed40781d8bac7adb61ed290c
wasky123 <wangshiqi35@163.com>
2017-09-14 20:26:24 -0400

    Add files via upload

---
+++ OneWordCount/WordCount.java
@@ -0,0 +1,34 @@
+package cn.hadoop.wc;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class WordCount {
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf);
+		
+	
+		job.setJarByClass(WordCount.class);
+			
+		job.setMapperClass(WCMapper.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(LongWritable.class);
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		
+		job.setReducerClass(WCReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(LongWritable.class);
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		job.waitForCompletion(true);
+	}
+
+}
